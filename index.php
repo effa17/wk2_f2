f31f2874a673bd2cc247d91c41a9fb470592392f
AinaNatasyaa <ainanatasya0703@gmail.com>
2023-09-05 16:09:48 +0800

    tambah index

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Text Area Input</title>
+</head>
+<body>
+
+<h3>Ceritakan tentang diri anda:</h3>
+<form method="POST" action="papar.php">
+    <textarea name="input_text" rows="5" cols="50"></textarea>
+    <br>
+    <input type="submit" name="submit" value="Simpan">
+</form>
+</body>
+</html>
+
+
